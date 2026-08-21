86ab89983e31aec372a7cbdbf3a1992614b2d91a
sujal <devsujal23@gmail.com>
2026-08-21 11:04:05 +0545

    fix: Correct class name from 'container' to 'containers' in index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 </head>
 <body>
 
-<div class="container">
+<div class="containers">
   <nav class="landing-nav">
     <a href="index.php" class="brand"><span class="brand-mark"><img src="img/Logo.png" alt="Hisaab Logo"></span> Hisaab</a>
     <div class="nav-actions">
